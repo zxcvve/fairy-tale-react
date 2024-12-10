eafcf938b636279ffa181eb96d4b0d0fe3764015
Serafim Lyovin <87949621+zxcvve@users.noreply.github.com>
2024-12-10 20:47:46 +0300

    Revert "Remove dynamic base path"
    This reverts commit 9b65e9ce363e8566176e7a48973920eb3fd8bbbf.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/fairy-tale-react/",
+  base: mode === "production" ? "/fairy-tale-react/" : "",
 }));
